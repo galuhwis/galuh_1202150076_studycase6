c59ea243364887f5b7f94c57611de2475d9373a9
galuhwis <atp.sports97@gmail.com>
2018-04-01 20:05:19 +0700

    Bismillah sukses

---
+++ app/src/main/java/com/example/galuhwisnupambayun/galuh_1202150076_studycase6/model/Comment.java
@@ -0,0 +1,32 @@
+package com.example.galuhwisnupambayun.galuh_1202150076_studycase6.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Comment {
+    public String uid;
+
+    public String author;
+
+    public String text;
+
+
+
+    public Comment() {
+
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+
+    }
+
+
+
+    public Comment(String uid, String author, String text) {
+
+        this.uid = uid;
+
+        this.author = author;
+
+        this.text = text;
+
+    }
+}
